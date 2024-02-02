60a7c3ecf8c174109c5bfc683bd6dcc6ad931027
Miles Talmey <mtalmey@equalexperts.com>
2024-02-02 20:16:03 +0000

    copyright, maven central pom

--- src/main/java/uk/emarte/reversevelocity/ParseException.java
+++ src/main/java/uk/emarte/reversevelocity/ParseException.java
@@ -1,6 +1,9 @@
+/*
+ * Copyright (C) 2017 Miles Talmey.
+ * Distributed under the MIT License (license terms are at http://opensource.org/licenses/MIT).
+ */
 package uk.emarte.reversevelocity;
 
-@SuppressWarnings("serial")
 public class ParseException extends Exception {
 	public ParseException(String string, Exception ex) {
 		super(string, ex);
--- src/test/java/uk/emarte/reversevelocity/test/ProductTypeMapper.java
+++ src/test/java/uk/emarte/reversevelocity/test/ProductTypeMapper.java
@@ -1,3 +1,7 @@
+/*
+ * Copyright (C) 2017 Miles Talmey.
+ * Distributed under the MIT License (license terms are at http://opensource.org/licenses/MIT).
+ */
 package uk.emarte.reversevelocity.test;
 
 import uk.emarte.reversevelocity.MappingException;
